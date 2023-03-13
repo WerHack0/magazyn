de54fab4b0a449d85e7a16ef02230979926358d5
WerHack0 <109349061+WerHack0@users.noreply.github.com>
2023-03-13 23:46:36 +0100

    Przycisk logowania

--- app/src/main/java/com/example/magazyn/MainActivity.kt
+++ app/src/main/java/com/example/magazyn/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.magazyn
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var loginButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
